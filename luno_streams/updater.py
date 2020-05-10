338d625ce95860583d491492c1772a42d92aa90b
erasmush <52200384+erasmush@users.noreply.github.com>
2020-05-10 20:11:14 +0200

    Update updater.py

--- luno_streams/updater.py
+++ luno_streams/updater.py
@@ -142,6 +142,6 @@ class Updater:
     def consolidated_order_book(self):
         
         return {
-            'bids': self.bids.values(),
-            'asks': self.asks.values(),
+            'bids': self.bids.values()[:5],
+            'asks': self.asks.values()[:5],
         }
