ff598e122beed1f9f4f6cc797908095adc0fd6d2
erasmush <52200384+erasmush@users.noreply.github.com>
2020-05-20 11:05:42 +0200

    Update updater.py

--- luno_streams/updater.py
+++ luno_streams/updater.py
@@ -141,14 +141,19 @@ class Updater:
 
     @property
     def consolidated_order_book(self):
-        Luno_BTC_bid_Orderbook_Dic = self.bids
-        Luno_BTC_bid_Orderbook_List = sorted(list(map(list, Luno_BTC_bid_Orderbook_Dic.items())), key=itemgetter([1][0]), reverse=True)
-        Luno_BTC_bid_Orderbook_List_Top_Bids = [Luno_BTC_bid_Orderbook_List[i] for i in range(0,len(Luno_BTC_bid_Orderbook_List)) if Luno_BTC_bid_Orderbook_List[i][1][0]==Luno_BTC_bid_Orderbook_List[0][1][0]]
+        #Luno_BTC_bid_Orderbook_Dic = self.bids
+        #Luno_BTC_bid_Orderbook_List = sorted(list(map(list, Luno_BTC_bid_Orderbook_Dic.items())), key=itemgetter([1][0]), reverse=True)
+        #Luno_BTC_bid_Orderbook_List_Top_Bids = [Luno_BTC_bid_Orderbook_List[i] for i in range(0,len(Luno_BTC_bid_Orderbook_List)) if Luno_BTC_bid_Orderbook_List[i][1][0]==Luno_BTC_bid_Orderbook_List[0][1][0]]
                                                     
-        Luno_BTC_ask_Orderbook_Dic = self.asks
-        Luno_BTC_ask_Orderbook_List = sorted(list(map(list, Luno_BTC_ask_Orderbook_Dic.items())), key=itemgetter([1][0]), reverse=False)
-        Luno_BTC_ask_Orderbook_List_Top_Asks = [Luno_BTC_ask_Orderbook_List[i] for i in range(0,len(Luno_BTC_ask_Orderbook_List)) if Luno_BTC_ask_Orderbook_List[i][1][0]==Luno_BTC_ask_Orderbook_List[0][1][0]]
+        #Luno_BTC_ask_Orderbook_Dic = self.asks
+        #Luno_BTC_ask_Orderbook_List = sorted(list(map(list, Luno_BTC_ask_Orderbook_Dic.items())), key=itemgetter([1][0]), reverse=False)
+        #Luno_BTC_ask_Orderbook_List_Top_Asks = [Luno_BTC_ask_Orderbook_List[i] for i in range(0,len(Luno_BTC_ask_Orderbook_List)) if Luno_BTC_ask_Orderbook_List[i][1][0]==Luno_BTC_ask_Orderbook_List[0][1][0]]
+        #return [
+        #     Luno_BTC_bid_Orderbook_List_Top_Bids,
+        #     Luno_BTC_ask_Orderbook_List_Top_Asks,
+        #]
         return [
-             Luno_BTC_bid_Orderbook_List_Top_Bids,
-             Luno_BTC_ask_Orderbook_List_Top_Asks,
+             self.bids,
+             self.asks,
         ]
+        
